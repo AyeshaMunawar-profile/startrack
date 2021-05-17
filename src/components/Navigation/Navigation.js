8c173d3b10d4e31d1ee4a085cb72c7d0eb827778
Ayesha <ayeshamunawarofficial@gmail.com>
2021-05-18 00:41:32 +0500

    Change the particle background

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <>
             <nav className="nav-bar">
-                <p className='f2 link dim underline pa4 pointer text-black'>
+                <p className='f2 link dim underline pa4 pointer text-off-white__0'>
                     Sign Out
                 </p>
             </nav>
