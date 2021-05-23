aa6cb6259da3f738b0c27d1be5043320fa6ee0d5
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2021-05-23 23:36:58 +0500

    Add datepicker in the sign up form

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './RegistrationForm.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const RegistrationForm = () => {
+    const [dateOfBirth, setdateOfBirth] = useState(new Date());
     return (
         <>
-            <div className="registration-form">
+            <div className="sign-up-form center py-3 px-2 ">
+                <Form className="text-off-white__0 fs-2 form form-transparent px-5 py-5 ">
+
+                    <FormText>
+                        <h1 className="fs-1 text-yellow__0 mb-5">Registration Form</h1>
+                    </FormText>
+
+                    <FormGroup className={"column-right"}>
+                        <Label for={"email"} className={"pe-5"}>Email Address</Label>
+                        <input className={"rounded-1 border-0 ps-2 px-5 fs-3 py-2 input input-email w-100"} name={"email"} type={"email"}
+                            id={"sign-up-email"}
+                            placeholder={"example@domain.com"} />
+                    </FormGroup>
+
+                    <FormGroup className={"column-right"}>
+                        <Label for={"date-of-birth"} className={"pe-5"}>Date Of Birth</Label>
+                        <DatePicker
+                            className="input input-date-of-birth rounded-1 border-0 ps-2 px-5 fs-3 py-2 w-100"
+                            selected={dateOfBirth}
+                            onChange={date => setdateOfBirth(date)}
+                        />
+                    </FormGroup>
+
+                    <FormGroup className={"column-right"}>
+                        <Label for={"password"} className={"pe-5"}>Password</Label>
+                        <input name={"password"} type={"password"} id={"sign-up-password"} placeholder={""}
+                            className={"rounded-1 border-0 ps-2 px-5 fs-3 py-2 input input-password w-100"} />
+                    </FormGroup>
+
+                    <FormGroup className={"column-right"}>
+                        <Label for={"confirm-password"} className={"pe-5"}>Confirm Password</Label>
+                        <input name={"confirm-password"} type={"confirm-password"} id={"sign-up-confirm-password"} placeholder={""}
+                            className={"rounded-1 border-0  ps-2 px-5 fs-3 py-2 input input-confirm-password w-100"} />
+                    </FormGroup>
+
+                    <FormGroup check>
+                        <Label check className={"fs-4"}>
+                            <Input className="input-remember-me" type="checkbox" className={"pl-0 mb-5 form-check-input"} />{' '}
+                            I accept the
+                            <a className="link link-color-yellow__-1 ps-3 pe-3">Terms of use</a>
+                            &
+                            <a className="link link-color-yellow__-1 ps-3 pe-3">Privacy Policy</a>
+                        </Label>
+                    </FormGroup>
+
+                    <Button type={"submit"}
+                        className={"button-color-yellow__0 text-off-white__0 px-5 py-3 center fs-3 btn-sign-up"}>Sign-up</Button>
+                </Form>
             </div>
         </>
     );
--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -6,26 +6,29 @@ const SignInForm = () => {
     return (
         <>
             <div className="sign-in-form center py-3 px-2 ">
-                <Form className="text-off-white__0 fs-2 w-auto form form-transparent px-5 py-5 ">
-                    <FormGroup className={"column-right"}>
+                <Form className="text-off-white__0 fs-2 w-auto form form-transparent px-5 py-5 mb-5">
+                    <FormText>
+                        <h1 className="fs-1 text-yellow__0 mb-5">Login</h1>
+                    </FormText>
+                    <FormGroup className={"column-right w-100"}>
                         <Label for={"email"} className={"pe-5"}>Email Address</Label>
-                        <input className={"rounded-1 border-0 px-5 fs-3 py-2 input "} name={"email"} type={"email"}
-                            id={"email"}
+                        <input className={"rounded-1 border-0 px-5 ps-2 fs-3 py-2 input "} name={"email"} type={"email"}
+                            id={"sign-in-email"}
                             placeholder={"example@domain.com"} />
                     </FormGroup>
-                    <FormGroup className={"column-right"}>
+                    <FormGroup className={"column-right w-100"}>
                         <Label for={"password"} className={"pe-5"}>Password</Label>
-                        <input name={"password"} type={"password"} id={"password"} placeholder={""}
-                            className={"rounded-1 border-0 px-5 fs-3 py-2 input"} />
+                        <input name={"password"} type={"password"} id={"sign-in-password"} placeholder={""}
+                            className={"rounded-1 border-0 px-5 ps-2 fs-3 py-2 input"} />
                     </FormGroup>
                     <FormGroup check>
                         <Label check className={"fs-4"}>
-                            <Input type="checkbox" className={"pl-0 mb-5"} />{' '}
+                            <Input type="checkbox" className={"pl-0 mb-5 form-check-input"} />{' '}
                             Remember me
                         </Label>
                     </FormGroup>
                     <Button type={"submit"}
-                        className={"button-color-yellow__0 text-off-white__0 px-5 py-3 center fs-3"}>Sign-in</Button>
+                        className={"button-color-yellow__0 text-off-white__0 px-5 py-3 center fs-3 btn-sign-in"}>Sign-in</Button>
                 </Form>
             </div>
         </>
